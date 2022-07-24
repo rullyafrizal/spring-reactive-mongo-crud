6df108d3c135100a8d249032a01ab971a8a8bba8
rullyafrizal <rullyafrizal4@gmail.com>
2022-07-24 14:45:28 +0700

    initial commit

---
+++ src/test/java/com/rully/spring/reactive/mongo/crud/SpringReactiveMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rully.spring.reactive.mongo.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
